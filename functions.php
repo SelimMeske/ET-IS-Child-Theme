e5ad1d38820fa56fcc9cae5aa0fde3c819170b12
Selim Mesic <mesic.selim@outlook.com>
2021-07-10 12:46:01 +0200

    Change version

--- functions.php
+++ functions.php
@@ -1,13 +1,13 @@
 <?php
 function my_custom_scripts() {
-    wp_enqueue_script( 'custom-js', get_stylesheet_directory_uri() . '/js/CamSkin.js', '3.0.0',true );
-    wp_enqueue_script( 'custom-js-2', get_stylesheet_directory_uri() . '/js/BackSkin.js', '3.0.0',true );
-    wp_enqueue_script( 'custom-js-3', get_stylesheet_directory_uri() . '/js/MobileObject.js', '3.0.0',true );
-    wp_enqueue_script( 'custom-js-4', get_stylesheet_directory_uri() . '/js/CreatedMobileObjects.js', '3.0.0',true );
-    wp_enqueue_script( 'custom-js-5', get_stylesheet_directory_uri() . '/js/main.js', '3.0.0',true );
-    wp_enqueue_script( 'cleaner-js', get_stylesheet_directory_uri() . '/js/cleaner.js', '3.0.0',true );
-    wp_enqueue_script( 'et-checkout-js', get_stylesheet_directory_uri() . '/js/et_checkout_handler.js', '3.0.0',true );
-    if (is_single(323)): wp_enqueue_style('clean-page', get_stylesheet_directory_uri() . '/clean_page_style.css', '3.0.0', false); endif;
+    wp_enqueue_script( 'custom-js', get_stylesheet_directory_uri() . '/js/CamSkin.js', rand(1, 100),true );
+    wp_enqueue_script( 'custom-js-2', get_stylesheet_directory_uri() . '/js/BackSkin.js', rand(1, 100),true );
+    wp_enqueue_script( 'custom-js-3', get_stylesheet_directory_uri() . '/js/MobileObject.js', rand(1, 100),true );
+    wp_enqueue_script( 'custom-js-4', get_stylesheet_directory_uri() . '/js/CreatedMobileObjects.js', rand(1, 100),true );
+    wp_enqueue_script( 'custom-js-5', get_stylesheet_directory_uri() . '/js/main.js', rand(1, 100),true );
+    wp_enqueue_script( 'cleaner-js', get_stylesheet_directory_uri() . '/js/cleaner.js', rand(1, 100),true );
+    wp_enqueue_script( 'et-checkout-js', get_stylesheet_directory_uri() . '/js/et_checkout_handler.js', rand(1, 100),true );
+    if (is_single(323)): wp_enqueue_style('clean-page', get_stylesheet_directory_uri() . '/clean_page_style.css', rand(1, 100), false); endif;
 }
 add_action( 'wp_enqueue_scripts', 'my_custom_scripts' );
 ?>
