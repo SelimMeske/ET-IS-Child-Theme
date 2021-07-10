3ea2c4f7a59a2ef34b254b0c81f55226b06ea141
Selim Mesic <mesic.selim@outlook.com>
2021-07-10 12:56:04 +0200

    Added style in enqueqe

--- functions.php
+++ functions.php
@@ -6,8 +6,9 @@ function my_custom_scripts() {
     wp_enqueue_script( 'custom-js-4', get_stylesheet_directory_uri() . '/js/CreatedMobileObjects.js', array(), rand(1, 100),true );
     wp_enqueue_script( 'custom-js-5', get_stylesheet_directory_uri() . '/js/main.js', array(), rand(1, 100),true );
     wp_enqueue_script( 'cleaner-js', get_stylesheet_directory_uri() . '/js/cleaner.js', array(), rand(1, 100),true );
-    wp_enqueue_script( 'et-checkout-js', get_stylesheet_directory_uri() . '/js/et_checkout_handler.js', array(), rand(1, 100),true );
-    if (is_single(323)): wp_enqueue_style('clean-page', get_stylesheet_directory_uri() . '/clean_page_style.css', array(), rand(1, 100), false); endif;
+    wp_enqueue_script( 'et-checkout-js', get_stylesheet_directory_uri() . '/js/et_checkout_handler.js', array(), rand(1, 100), true );
+    if (is_single(323)): wp_enqueue_style('clean-page', get_stylesheet_directory_uri() . '/clean_page_style.css', array(), rand(1, 100), 'all'); endif;
+    if (is_single(323)): wp_enqueue_style('et-main-style', get_stylesheet_directory_uri() . '/style.css', array(), rand(1, 100), 'all'); endif;
 }
 add_action( 'wp_enqueue_scripts', 'my_custom_scripts' );
 ?>
