c75df8bfd214d6b6efbd19b7bd0d4cb047ad5fe3
Selim Mesic <mesic.selim@outlook.com>
2021-07-11 22:15:12 +0200

    Major update 62634324

--- js/CartItemsHandler.js
+++ js/CartItemsHandler.js
@@ -1,3 +1,5 @@
+let currentCartItems = [];
+
 jQuery.ajax({
   url: "https://infinityskins.ba/wp-json/cocart/v2/cart/items",
   method: "GET",
@@ -6,7 +8,6 @@ jQuery.ajax({
   complete: function (response) {
     console.log(response.responseJSON)
     let odgovor = response.responseJSON;
-    let currentCartItems = [];
     for (let i = 0; i < Object.keys(odgovor).length; i++) {
       currentCartItems.push(Object.values(odgovor)[i].id);
     }
