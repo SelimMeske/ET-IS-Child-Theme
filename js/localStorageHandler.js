4a3a871049e6c047023ed5d93eade27ea71c21f7
Selim Mesic <mesic.selim@outlook.com>
2021-07-17 13:45:57 +0200

    Testing new meta 2 354235237567

--- js/localStorageHandler.js
+++ js/localStorageHandler.js
@@ -1,6 +1,6 @@
 function addMetaToLocal(object){
   console.log(object);
   let currentLocalMetaList = JSON.parse(window.localStorage.getItem('cart_meta'));
-  console.log(typeof currentLocalMetaList);
+  return typeof currentLocalMetaList;
   console.log(currentLocalMetaList);
 }
