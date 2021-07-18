39882513a5674055a229fabc0d2ed41d62b5a1dd
SelimMeske <mesic.selim@outlook.com>
2021-07-18 12:35:03 +0200

    load cleaner only on our page bug fix 2

--- js/cleaner.js
+++ js/cleaner.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function() {
 	let all_empty_p_tags = document.querySelectorAll('p');
 
 	for (let i = 0; i < all_empty_p_tags.length; i++) {
-		if(all_empty_p_tags[i].innerText == ""){
+		if(all_empty_p_tags[i].innerText == "" && all_empty_p_tags[i].innerHTML == ""){
 			all_empty_p_tags[i].remove();
 		}
 	}
