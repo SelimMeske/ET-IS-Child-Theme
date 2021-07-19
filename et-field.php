f9011d2fcb9a5a23422f7e07df815d5e6d10fb05
SelimMeske <mesic.selim@outlook.com>
2021-07-19 13:12:35 +0200

    added et test php field 334476543

--- et-field.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-function add_my_field() { ?>
-
-<div class="my-field">
-    <input type="text" name="my-filed" value=''>
-</div>
-
-<?php }
--- functions.php
+++ functions.php
@@ -1,7 +1,5 @@
 <?php
 
-require_once(get_stylesheet_directory_uri() . '/et-field.php');
-
 function my_custom_scripts() {
     wp_enqueue_script( 'local-storage-handler', get_stylesheet_directory_uri() . '/js/localStorageHandler.js', array('jquery'), rand(100, 1000) . '.' . rand(0, 1000),true );
     wp_enqueue_script( 'cart-meta-object', get_stylesheet_directory_uri() . '/js/CartMetaObject.js', array('jquery'), rand(100, 1000) . '.' . rand(0, 1000),true );
@@ -19,5 +17,23 @@ function my_custom_scripts() {
     if (is_single(88)): wp_enqueue_style('chart-cleaner', get_stylesheet_directory_uri() . '/chart_cleaner.css', array(), rand(100, 1000) . '.' . rand(0, 1000), 'all'); endif;
 }
 add_action( 'wp_enqueue_scripts', 'my_custom_scripts' );
-add_action('woocommerce_before_add_to_cart_button', 'add_my_field');
+
+add_action('woocommerce_before_add_to_cart_button', 'hidden_input_field');
+
+function hidden_input_field() { ?>
+    <input type="text" id="back-skin-material" name="back-skin-material" value=''>
+    <input type="text" id="cam-skin-material" name="cam-skin-material" value=''>
+} <?php
+
+
+add_filter('woocommerce_add_to_cart_validation', 'no_skin_error', 10, 3);
+function no_skin_error($passed, $product_id, $qty) {
+    if(isset($_POST['back-skin-material']) && isset($_POST['cam-skin-material']) && sanitize_text_field($_POST['back-skin-material']) && sanitize_text_field($_POST['cam-skin-material'])  == '') {
+        wc_add_notice( 'Molim vas odaberite skin.', 'error' );
+        
+        $passed = false;
+    }
+    return $passed;
+}
+
 ?>
