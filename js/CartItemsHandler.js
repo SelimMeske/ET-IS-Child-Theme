37881fa93cabafa0cdb5bc2ae71e8cb4b5e5f0f2
SelimMeske <mesic.selim@outlook.com>
2021-07-20 23:26:54 +0200

    bug fix

--- js/CartItemsHandler.js
+++ js/CartItemsHandler.js
@@ -1,3 +1,12 @@
+let currentCartItems = [];
+let backMainText = 'Pozadniski skin';
+let camMainText = 'Skin kamere';
+let bcMainText = 'Pozadniski + kamera skin';
+
+if(window.location.pathname == '/cart/'){
+  document.querySelector('#post-87').style.display = 'none';
+}
+
 jQuery.ajax({
   url: "https://infinityskins.ba/wp-json/cocart/v2/cart/items",
   method: "GET",
@@ -5,6 +14,12 @@ jQuery.ajax({
   contentType: "application/json; charset=utf-8",
   complete: function (response) {
     let odgovor = response.responseJSON;
-    console.log(odgovor);
+
+    cleanMetaDataInLocal(odgovor);
+    
+    for (let i = 0; i < Object.keys(odgovor).length; i++) {
+      let currentResponseId = Object.values(odgovor)[i].id;
+      currentCartItems.push(currentResponseId.toString());
+    }
   }
 });
