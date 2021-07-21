7cff22000716fbf2e67eafe0eb97f1d5ad569305
SelimMeske <mesic.selim@outlook.com>
2021-07-21 09:54:22 +0200

    bug fix3433432d234132132gdf

--- functions.php
+++ functions.php
@@ -77,9 +77,118 @@ function show_in_table($cart_item, $order_item){
 }
 
 function main_template_shortcode() {
-    ob_start();
-    include(get_stylesheet_directory_uri() . '/main_template.php');
-    $content = ob_get_contents();
+   $content = '<div class="app et-d-flex">
+    <div class="et-container et-d-flex mobile-only-flex">
+        <div class="et-mobile-container">
+            <figure id="et-figure" class="et-d-flex">
+                <div class="inner-holder">
+                    <svg style="background: #0c0c0c;" class="main-svg" viewBox="0 0 335 713" width="335" height="720">
+                        <image width="335" height="720" class="main-image"
+                            href="http://infinityskins.ba/wp-content/uploads/2021/06/base-3.jpg"></image>
+                        <image class="cam-image" width="94" height="165" x="33" y="27" href=""></image>
+                        <image class="back-image" width="335" height="713" x="0" y="2" href=""></image>
+                    </svg>
+                </div>
+            </figure>
+        </div>
+        <div class="et-picker-container et-d-flex">
+            <div class="et-card et-d-flex"
+                style="margin-top: 10px; padding: 0px; background: transparent; border-bottom: none;">
+                <small style="margin-right: 10px;">Unesite vaš</p>
+                    <p>model&nbsp;telefona</p>
+                    <p>
+                </small></p>
+                <div class="et-sub-container et-d-flex"
+                    style="flex-direction: column; width: 100%; border-radius: 5px;"><input id="phone-model"
+                        style="color: white;" type="text" placeholder="Npr. Samsung A10" /></div>
+                </p>
+            </div>
+            <div id="et-back-skin" class="et-card et-card-back-skin et-d-flex"
+                style="font-size: 18px; justify-content: space-between;">
+                <p style="color: #9d94c9;"><strong>POZADINA</strong></p>
+                <div class="et-sub-container et-d-flex">
+                    9.90 KM</p>
+                    <p class="et-d-flex et-button-style et-add-ribbon et-d-flex">DODAJ</p>
+                    </p>
+                </div>
+                <div class="clicable-et-card"></div>
+                </p>
+            </div>
+            <div id="et-cam-skin" class="et-card et-card-camera-skin et-d-flex"
+                style="font-size: 18px; justify-content: space-between;">
+                <p style="color: #9d94c9;"><strong>KAMERA</strong></p>
+                <div class="et-sub-container et-d-flex">
+                    2.90 KM</p>
+                    <p class="et-d-flex et-button-style et-add-ribbon et-d-flex">DODAJ</p>
+                    </p>
+                </div>
+                <div class="clicable-et-card"></div>
+                </p>
+            </div>
+            <div class="et-card-buy-button et-card et-d-flex" style="font-size: 18px; justify-content: flex-end;">
+                <div class="et-sub-container et-d-flex">
+                    <p class="et-d-flex et-button-style et-add-ribbon et-buy-button">
+                        <span class="cart-button-image"><br />
+                            <img src="https://infinityskins.ba/wp-content/uploads/2021/06/shopping-cart-2.png"
+                                alt="" /><br />
+                        </span><br />
+                        Kupi
+                    </p>
+                    </p>
+                </div>
+                </p>
+            </div>
+            <p>
+                <!--Choose Skin Popup Window-->
+            </p>
+            <div class="et-main-popup-window et-d-flex et-close-popup-window">
+                <div class="et-section section-one">
+                    <div class="et-back-button et-d-flex"><img
+                            src="https://infinityskins.ba/wp-content/uploads/2021/06/go-back-arrow.png" alt=""
+                            width="25" /></div>
+                    <div class="et-choose-style-title et-d-flex" style="flex-direction: column;"><small>Pozadinski
+                            Skinovi</small><strong>ODABERI STIL</strong></div>
+                    </p>
+                </div>
+                <div class="et-section section-two">
+                    <ul>
+                        <li class="material-category">
+                            <div id="carbon-gray" class="material-card"><img
+                                    src="https://infinityskins.ba/wp-content/uploads/2021/06/carbon-gray-sample.jpg"
+                                    alt="" /></div>
+                            <div id="carbon-red" class="material-card"><img
+                                    src="https://infinityskins.ba/wp-content/uploads/2021/05/Screenshot_2.png" alt="" />
+                            </div>
+                            <div id="carbon-black" class="material-card"><img
+                                    src="https://infinityskins.ba/wp-content/uploads/2021/05/Screenshot_4.png" alt="" />
+                            </div>
+                            <div id="carbon-white" class="material-card"><img
+                                    src="https://infinityskins.ba/wp-content/uploads/2021/06/carbon-white-sample.jpg"
+                                    alt="" /></div>
+                            <div id="carbon-yellow" class="material-card"><img
+                                    src="https://infinityskins.ba/wp-content/uploads/2021/06/yellowCarbon-Sample.jpg"
+                                    alt="" /></div>
+                        </li>
+                        <li class="material-category"></li>
+                        <li class="material-category"></li>
+                        <li class="material-category"></li>
+                        <li class="material-category"></li>
+                    </ul>
+                </div>
+                <div class="et-d-flex et-section section-three" style="justify-content: space-between;">
+                    <div class="et-d-flex et-delete-material"><img
+                            src="https://infinityskins.ba/wp-content/uploads/2021/06/delete.png" alt="" width="20" />
+                    </div>
+                    <div class="et-d-flex et-button et-button-style et-add-skin-button">ODABERI</div>
+                    </p>
+                </div>
+                </p>
+            </div>
+            </p>
+        </div>
+        </p>
+    </div>
+</div>'
 
     return $content;
 }
