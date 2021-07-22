a3e2cd04cb89b250acd146782ad6c20b7161610e
SelimMeske <mesic.selim@outlook.com>
2021-07-22 22:44:30 +0200

    Updated the image

--- js/CreatedMobileObjects.js
+++ js/CreatedMobileObjects.js
@@ -5,11 +5,11 @@ let galaxyS21 = new Mobile("Galaxy S21",
                             "http://infinityskins.ba/wp-content/uploads/2021/06/carbon-yellow-5.png",
                             "http://infinityskins.ba/wp-content/uploads/2021/06/carbon-white-5.png",
                             "http://infinityskins.ba/wp-content/uploads/2021/06/carbon-gray.png",
-                            "http://infinityskins.ba/wp-content/uploads/2021/07/back-skin-tigar-png.png",
+                            "http://infinityskins.ba/wp-content/uploads/2021/07/leopard-back-new.png",
                             "http://infinityskins.ba/wp-content/uploads/2021/06/carbon-red.png",
                             "http://infinityskins.ba/wp-content/uploads/2021/06/carbon-black-3.png",
                             "http://infinityskins.ba/wp-content/uploads/2021/06/infinity-logo.png",
                             "http://infinityskins.ba/wp-content/uploads/2021/06/carbon-white-cam-new.png",
                             "http://infinityskins.ba/wp-content/uploads/2021/06/carbon-gray-camera-new.png",
-                            "http://infinityskins.ba/wp-content/uploads/2021/07/png-kamera-tigar.png"
+                            "http://infinityskins.ba/wp-content/uploads/2021/07/leopard-cam-new.png"
                             );
