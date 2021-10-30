556767c2381a167a8957d6b8624396bcfadf02c8
Selim Mesic <mesic.selim@outlook.com>
2021-10-30 18:25:08 +0200

    Fixed Bugs

--- js/MobileObject.js
+++ js/MobileObject.js
@@ -2,7 +2,7 @@ class Mobile extends BackSkins{
     constructor(name, carbonRedBack, carbonBlackBack, carbonYellowBack, carbonWhiteBack, carbonGrayBack, leopardBack, blueCamoBack, threeDCubeBack, woodBack, supremeBack, summerBack, snakeBack,
                 nikeBack, louisVuittonBack, lighPinkBack, lighPinkV2Back, honeyCombBack, honeyCombv2, hexagonBack, blueMarbelBack, artBack,
         carbonRedCam, carbonBlackCam, leatherBlackCam, whiteCarbonCam, carbonGrayCam, leopardCam){
-        super(carbonRedBack, carbonBlackBack, carbonYellowBack, carbonWhiteBack, carbonGrayBack, leopardBack, blueCamoBack, 3DCubeBack, woodBack, supremeBack, summerBack, snakeBack,
+        super(carbonRedBack, carbonBlackBack, carbonYellowBack, carbonWhiteBack, carbonGrayBack, leopardBack, blueCamoBack, threeDCubeBack, woodBack, supremeBack, summerBack, snakeBack,
                     nikeBack, louisVuittonBack, lighPinkBack, lighPinkV2Back, honeyCombBack, honeyCombv2, hexagonBack, blueMarbelBack, artBack,
               carbonRedCam, carbonBlackCam, leatherBlackCam, whiteCarbonCam, carbonGrayCam, leopardCam);
         this.name = name;
