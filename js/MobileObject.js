02fd3f55125078479c70d58570fde6d5b2b6d584
Selim Mesic <mesic.selim@outlook.com>
2021-10-30 16:48:46 +0200

    Added bug fix

--- js/MobileObject.js
+++ js/MobileObject.js
@@ -1,5 +1,5 @@
 class Mobile extends BackSkins{
-    constructor(name, carbonRedBack, carbonBlackBack, carbonYellowBack, carbonWhiteBack, carbonGrayBack, leopardBack,
+    constructor(name, carbonRedBack, carbonBlackBack, carbonYellowBack, carbonWhiteBack, carbonGrayBack, leopardBack, blueCamoBack,
         carbonRedCam, carbonBlackCam, leatherBlackCam, whiteCarbonCam, carbonGrayCam, leopardCam){
         super(carbonRedBack, carbonBlackBack, carbonYellowBack, carbonWhiteBack, carbonGrayBack, leopardBack,
               carbonRedCam, carbonBlackCam, leatherBlackCam, whiteCarbonCam, carbonGrayCam, leopardCam);
